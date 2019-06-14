726c748efde62c9a44226a60c7ab8f8cb00b077e
ashwinirmerukar <ashwinimerukar10@gmail.com>
2019-06-14 22:32:07 +0530

    first commit

---
+++ src/test/java/com/spring/springtest/SpringtestApplicationTests.java
@@ -0,0 +1,12 @@
+package com.spring.springtest;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class SpringtestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
